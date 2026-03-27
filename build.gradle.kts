67912565c7344834c19d9e2f7cd09bb7e1cc9de6
Michael Schout <schoutm@gmail.com>
2026-03-27 14:37:23 -0500

    Set up publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,11 @@
-plugins { id("mschout.all-conventions") }
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinJvm
+
+plugins {
+  id("mschout.all-conventions")
+  alias(libs.plugins.dokka)
+  alias(libs.plugins.maven.publish)
+}
 
 group = "io.github.mschout"
 
@@ -12,4 +19,43 @@ dependencies {
   testImplementation(kotlin("test"))
   testImplementation(libs.kotest.runner.junit5)
   testImplementation(libs.kotest.assertions.core)
+}
+
+mavenPublishing {
+  configure(KotlinJvm(javadocJar = JavadocJar.Dokka("dokkaGenerateModuleHtml")))
+
+  publishToMavenCentral()
+
+  signAllPublications()
+
+  coordinates(group.toString(), "microsoft-json-date-parser", version.toString())
+
+  pom {
+    name.set("Jackson Datatype MS Date")
+    description.set(
+        "Java/Kotlin OffsetDateTime parser for Microsoft JSON date format (/Date(ticks+offset)/) strings"
+    )
+    url.set("https://github.com/mschout/microsoft-json-date-parser")
+
+    licenses {
+      license {
+        name.set("The Apache License, Version 2.0")
+        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+      }
+    }
+
+    developers {
+      developer {
+        id.set("mschout")
+        name.set("Michael Schout")
+        url.set("https://github.com/mschout")
+      }
+    }
+
+    scm {
+      url.set("https://github.com/mschout/microsoft-json-date-parser")
+      connection.set("scm:git:git://github.com/mschout/microsoft-json-date-parser.git")
+      developerConnection.set("scm:git:ssh://git@github.com/mschout/microsoft-json-date-parser.git")
+    }
+  }
 }
